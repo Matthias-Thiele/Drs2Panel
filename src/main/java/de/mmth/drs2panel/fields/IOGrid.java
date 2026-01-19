f50d5ebb3445219d4d870459e761efda0eaa26b5
Matthias-Thiele <matthias@mmth.de>
2026-01-19 11:22:34 +0100

    Simulation Relaisblock verbessert.

--- src/main/java/de/mmth/drs2panel/fields/IOGrid.java
+++ src/main/java/de/mmth/drs2panel/fields/IOGrid.java
@@ -23,6 +23,8 @@ public class IOGrid extends GridPane {
   
   private final Drs2 drs2;
   private final List<Label> lampList = new ArrayList<>();
+  private Button vorblockAH;
+  private Button rückblockAH;
   
   public IOGrid(Drs2 drs2) {
     this.drs2 = drs2;
@@ -38,8 +40,19 @@ public class IOGrid extends GridPane {
           var state = (LabelState)lamp.getUserData();
           var lampState = drs2.getLampState(state.ioId);
           if (state.isActive != lampState) {
+            System.out.println("Lamp state " + state.ioId + " changed to " + state.isActive);
             state.isActive = lampState;
             lamp.setStyle(lampState ? "-fx-background-color: red": "-fx-background-color: white");
+            
+            if (lampState) {
+              if (state.assigned == vorblockAH) {
+                processButton(vorblockAH, ButtonAction.SET);
+              } else if (state.assigned == rückblockAH) {
+                processButton(rückblockAH, ButtonAction.RESET);
+              } else if (state.assigned != null) {
+                processButton(state.assigned, ButtonAction.INVERT);
+              }
+            }
           }
         }
       }
@@ -55,33 +68,35 @@ public class IOGrid extends GridPane {
     addInput("Fa F", Const.SCHLUESSEL_F, -1, false);
     addInput("SW I", Const.WSCHLUESSEL1, Const.SlFT1Relais, false);
     addInput("SW IV", Const.WSCHLUESSEL4, Const.SlFT4Relais, false);
-    addInput("Nach AH", Const.BLOCK_AH_OUT, -1, true);
-    addInput("Von AH", Const.BLOCK_AH_IN, - 1, true);
+    vorblockAH = addInput("Nach AH", Const.BLOCK_AH_OUT, -1, true);
+    rückblockAH = addInput("Von AH", Const.BLOCK_AH_IN, - 1, true);
   }
   
   private void addLamps() {
     var label = new Label("Ausgaben");
     this.add(label, nextLabelCol++, 1);
     
-    this.addLamp(" VB nach AH", Const.BlockMOut);
-    this.addLamp(" RB von AH", Const.BlockMIn);
+    this.addLamp(" VB nach AH", Const.StreckeNachAH, vorblockAH);
+    this.addLamp(" RB von AH", Const.StreckeVonAH, rückblockAH);
   }
   
-  private void addLamp(String name, int ioId) {
+  private void addLamp(String name, int ioId, Button assignedOperation) {
     var label = new Label(name);
     label.setPrefWidth(Presets.FIELD_WIDTH - 10);
     label.setPrefHeight(Presets.MIDDLE_BAR_HEIGHT);
     
     label.setStyle("-fx-background-color: white");
-    var ls = new LabelState(false, ioId);
+    var ls = new LabelState(false, ioId, assignedOperation);
     label.setUserData(ls);
     this.add(label, nextLabelCol++, 1);
+    lampList.add(label);
   }
   
-  private void addInput(String name, int ioId, int checkId, boolean isBlock) {
+  private Button addInput(String name, int ioId, int checkId, boolean isBlock) {
     var bt = new Button(name);
     var state = new ButtonState();
     state.isPressed = false;
+    state.isBlockButton = isBlock;
     state.ioId = ioId;
     state.checkId = checkId;
     bt.setUserData(state);
@@ -99,30 +114,50 @@ public class IOGrid extends GridPane {
         }
       }
       
-      s.isPressed = !s.isPressed;
-      if (isBlock) {
-        bt.setStyle(s.isPressed ? "-fx-background-color: red": "-fx-background-color: white");
-      } else {
-        bt.setStyle(s.isPressed ? "-fx-background-color: lightgreen": "");
-      }
-      drs2.setSwitch(s.ioId, s.isPressed);
+      processButton(bt, ButtonAction.INVERT);
     });
     this.add(bt, nextButtonCol++, 0);
+    
+    return bt;
   }
   
+  private void processButton(Button bt, ButtonAction action) {
+    ButtonState s = (ButtonState) bt.getUserData();
+    switch (action) {
+      case SET: s.isPressed = true; break;
+      case RESET: s.isPressed = false; break;
+      case INVERT: s.isPressed = !s.isPressed; break;
+    }
+    
+    if (s.isBlockButton) {
+      bt.setStyle(s.isPressed ? "-fx-background-color: red": "-fx-background-color: white");
+    } else {
+      bt.setStyle(s.isPressed ? "-fx-background-color: lightgreen": "");
+    }
+    drs2.setSwitch(s.ioId, s.isPressed);
+  }
+  
+  enum ButtonAction {
+    SET,
+    RESET,
+    INVERT
+  }
   class ButtonState {
     boolean isPressed;
+    boolean isBlockButton;
     int ioId;
     int checkId;
   }
   
   class LabelState {
-    LabelState(boolean isActive, int ioId) {
+    LabelState(boolean isActive, int ioId, Button assigned) {
       this.isActive = isActive;
       this.ioId = ioId;
+      this.assigned = assigned;
     }
     
     boolean isActive;
     int ioId;
+    Button assigned;
   }
 }
